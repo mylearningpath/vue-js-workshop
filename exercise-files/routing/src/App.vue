a5f16a3eaa604a2f86362558dcbd2229d68300c8
Ronalson Filho <contact@ronalson.com.br>
2017-09-18 20:35:37 -0300

    Add navbar to App.vue

--- exercise-files/routing/src/App.vue
+++ exercise-files/routing/src/App.vue
@@ -1,9 +1,18 @@
 <template>
+  <header class="app-header">
+      <app-nav></app-nav>
+    </header>
   <main class="container">
     <router-view></router-view>
   </main>
 </template>
 
 <script>
-export default {}
+import AppNav from './components/AppNav'
+
+export default {
+  components: {
+    AppNav
+  }
+}
 </script>
